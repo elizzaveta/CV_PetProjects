6f68a082bfb1ade69234c5194afbc6b05d7b0682
elizzaveta <y.timchenko-2025@kpi.com>
2023-02-03 14:11:17 +0200

    Added contact and source code links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Box, Container, ThemeProvider, Typography} from "@mui/material";
 import {theme} from "../styles/Theme";
 import styles from "../styles/css/Foooter.module.css"
+import LinkIcon from "./LinkIcon";
+import {images} from "../utils/Images";
 
 const Footer = () => {
     return (
@@ -9,14 +11,46 @@ const Footer = () => {
             <footer>
                 <Container>
                     <Box sx={{
-                        padding: '50px 0px 0px 0px',
+                        padding: '50px 0px 20px 0px',
                         display: 'flex',
-                        flexDirection: 'column',
                         alignItems: 'center',
-                        justifyContent: 'center'
+                        justifyContent: 'space-around'
                     }}>
-                        <Typography>Copyright: <a href='https://github.com/elizzaveta'>elizavetta</a></Typography>
-                        <Typography>2023</Typography>
+                        <Box sx={{
+                            padding: '5px 0px 0px 0px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'space-around'
+                        }}>
+                            <Typography variant="h3">Contact</Typography>
+                            <Box sx={{
+                                padding: '5px 0px 0px 0px',
+                                display: 'flex',
+                                gap: '15px',
+                                alignItems: 'center',
+                                justifyContent: 'space-around'
+                            }}>
+                                {images.footerIcons.contact.map((iconData)=>
+                                    <LinkIcon image={iconData.icon} linkTo={iconData.link}/>
+                                )}
+                            </Box>
+
+                        </Box>
+                        <Box sx={{
+                            padding: '5px 0px 0px 0px',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'space-around'
+                        }}>
+                            <Typography variant="h3">Source Code</Typography>
+                            <div>
+                                <LinkIcon image={images.footerIcons.sourceCode.icon} linkTo={images.footerIcons.sourceCode.link}/>
+
+                            </div>
+
+                        </Box>
                     </Box>
 
                 </Container>
--- src/utils/Images.js
+++ src/utils/Images.js
@@ -6,6 +6,13 @@ import cssIcon from "../images/icons/cssIcon.png"
 import jsIcon from "../images/icons/jsIcon.png"
 import figmaIcon from "../images/icons/figmaIcon.png"
 
+import telegramIcon from "../images/icons/telegramIcon.png"
+import emailIcon from "../images/icons/emailIcon.png"
+import linkedinIcon from "../images/icons/linkedinIcon.png"
+import githubIcon from "../images/icons/githubIcon.png"
+
+
+
 export const images = {
     technologies: [
         {
@@ -37,5 +44,26 @@ export const images = {
             name: "Figma",
             icon: figmaIcon
         }
-    ]
+    ],
+    footerIcons:{
+        contact:[
+            {
+                icon: telegramIcon,
+                link: "https://t.me/elizavetvet"
+            },
+            {
+                icon: emailIcon,
+                link: "mailto:yelizaveta.timchenko21@gmail.com"
+            },
+            {
+                icon: linkedinIcon,
+                link: "https://www.linkedin.com/in/elizavetvet"
+            }
+        ],
+        sourceCode:{
+            icon: githubIcon,
+            link: "https://github.com/elizzaveta/CV_PetProjects"
+        }
+
+    }
 }
